85899be3bb4fd8734ba8999eaa1ee154a7653504
Guilherme Frota <guiquintelas96@gmail.com>
2020-06-19 19:52:39 -0300

    make PersonRepository save in disk

--- BLL/Repositories/PersonRepository.cs
+++ BLL/Repositories/PersonRepository.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using BLL.Models;
 
@@ -6,15 +6,16 @@ namespace BLL.Repositories
 {
     public class PersonRepository
     {
-        private static readonly List<Person> AllPersons = new List<Person>()
+        private static readonly string FilePath = Path.Combine(Environment.CurrentDirectory, "../../Res/db.csv");
+        public PersonRepository()
         {
-            new Person()
+            this.LoadFromFile();
+        }
+
+        public void Dispose()
             {
-                FirstName = "Teste",
-                LastName = "Opa",
-                BirthDate = DateTime.Parse("1996-10-07")
-            },
-            new Person()
+            this.WriteToFile();
+        }
             {
                 FirstName = "Teste 2",
                 LastName = "Opa",
@@ -37,7 +38,37 @@ namespace BLL.Repositories
         
         public void Add(Person person)
         {
-            AllPersons.Add(person);
+        private void LoadFromFile()
+        {
+            using (var stream = new StreamReader(FilePath, Encoding.Default))
+            {
+                while (!stream.EndOfStream)
+                {
+                    var row = stream.ReadLine()?.Split(';');
+                    if (row != null)
+                    {
+                        _allPersons.Add(new Person()
+                        {
+                            Id = row[0],
+                            FirstName = row[1],
+                            LastName = row[2],
+                            BirthDate = DateTime.Parse(row[3]),
+                        });
+                    }
+                }
+            }
+        }
+        
+        private void WriteToFile()
+        {
+            Console.WriteLine("Salvando dados em disco ...");
+            using (var stream = new StreamWriter(FilePath))
+            {
+                foreach (var person in _allPersons)
+                {
+                    stream.WriteLine($"{person.Id};{person.FirstName};{person.LastName};{person.BirthDate:yyyy-MM-dd}");
+                }
+            }
         }
     }
 }
--- BirthdayConsole/App.cs
+++ BirthdayConsole/App.cs
@@ -1,19 +1,34 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using AniversarioConsole.Services;
 using BLL.Repositories;
 
 namespace AniversarioConsole
 {
-    internal class App
+    internal static class App
     {
-        public static readonly MenuService MenuService = new MenuService();
+        private static readonly MenuService MenuService = new MenuService();
         public static readonly PersonRepository PersonRepository = new PersonRepository();
+
+        private static bool _isDisposing;
 
         public static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.ProcessExit += Dispose;
+            Console.CancelKeyPress += Dispose;
+            
             Console.Write("Bem Vindo - Gerenciador de Aniversários");
             MenuService.ShowMenu();
+        }
+
+        private static void Dispose(object sender, object e)
+        {
+            if (_isDisposing)
+                return;
+
+            _isDisposing = true;
+            
+            PersonRepository.Dispose();
+            Console.WriteLine ("Tchau tchau!");
         }
     }
 }
